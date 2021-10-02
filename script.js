a0e7d8f6a5604fa896daa707501d09aed87f641f
xvtlnx <65411175+xvtlnx@users.noreply.github.com>
2021-10-02 21:45:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 menu.onclick = function myFunction() {
-    var x = document.getElementById('myTopnav');
+    let x = document.getElementById('myTopnav');
     if (x.className === "topnav") {
         x.className += " responsive";
     }
